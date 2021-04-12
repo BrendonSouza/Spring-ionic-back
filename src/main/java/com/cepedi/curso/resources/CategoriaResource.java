c42e3fdbf43484f17df2cd4664058c5e9a8e06ba
BrendonSouza <brendonqueiros@hotmail.com>
2021-04-12 15:07:24 -0300

    :wolf: paginaçao de categorias

--- src/main/java/com/cepedi/curso/resources/CategoriaResource.java
+++ src/main/java/com/cepedi/curso/resources/CategoriaResource.java
@@ -9,11 +9,13 @@ import com.cepedi.curso.dto.CategoriaDTO;
 import com.cepedi.curso.services.CategoriaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -63,6 +65,20 @@ public class CategoriaResource {
 
     List<Categoria> list = service.findAll();
     List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+    return ResponseEntity.ok().body(listDTO);
+
+  }
+
+  @RequestMapping(value = "/page", method = RequestMethod.GET)
+  // PathVariable serve para passar o que vier na url pra dentro do obj
+  public ResponseEntity<Page<CategoriaDTO>> findPage(
+    @RequestParam(value="page",defaultValue = "0") Integer page,
+    @RequestParam(value="linesPerPage",defaultValue = "24")Integer linesPerPage, 
+    @RequestParam(value="orderBy",defaultValue = "nome") String orderBy, 
+    @RequestParam(value="direction",defaultValue = "ASC")String direction) {
+
+    Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+    Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));
     return ResponseEntity.ok().body(listDTO);
 
   }
