ece3d7031a2010d1301b67e086095f09b376c231
BrendonSouza <brendonqueiros@hotmail.com>
2021-04-12 16:37:36 -0300

    :wrench: Refatorando a exibiçao das validaçoes

---
+++ src/main/java/com/cepedi/curso/resources/exceptions/ValidationError.java
@@ -0,0 +1,32 @@
+package com.cepedi.curso.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+
+  
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = 1L;
+  
+  private List<FieldMessage> errors =new ArrayList<>();
+
+  public ValidationError(Integer status, String msg, Long timestamps) {
+    super(status, msg, timestamps);
+    
+  }
+
+  public List<FieldMessage> getErrors() {
+    return errors;
+  }
+
+  public void addError(String fieldName,String mensagem) {
+    errors.add(new FieldMessage(fieldName,mensagem));
+  }
+
+
+  
+}
