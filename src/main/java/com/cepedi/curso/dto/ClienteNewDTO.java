d2265b75c0e7b33614111f331e17bfcc22523385
BrendonSouza <brendonqueiros@hotmail.com>
2021-04-13 15:34:58 -0300

    :wolf: fazendo a validaçao do cpfCNPJ customizada

--- src/main/java/com/cepedi/curso/dto/ClienteNewDTO.java
+++ src/main/java/com/cepedi/curso/dto/ClienteNewDTO.java
@@ -2,26 +2,44 @@ package com.cepedi.curso.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import com.cepedi.curso.services.validations.ClienteInsert;
+
+import org.hibernate.validator.constraints.Length;
 
 
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
   /**
    *
    */
   private static final long serialVersionUID = 1L;
  
-
+  @NotEmpty(message="Nome não pode ser vazio")
+  @Length(min=5,max=120,message="O nome deve ter entre 5 e 120 char")
   private String nome;
+
+  @NotEmpty(message="email não pode ser vazio")
+  @Email(message="Email inválido")
   private String email;
+
+  @NotEmpty(message="CPF ou CNPJ não pode ser vazio")
   private String cpfOuCnpj;
   private Integer tipo;
  
+  @NotEmpty(message="logradouro não pode ser vazio")
   private String logradouro;
+  @NotEmpty(message="Numero não pode ser vazio")
   private String numero;
+
   private String complemento;
   private String bairro;
+  @NotEmpty(message="CEP não pode ser vazio")
   private String cep;
 
+ @NotEmpty(message="telefone1 não pode ser vazio")
   private String telefone1;
   private String telefone2;
   private String telefone3;
@@ -40,180 +58,104 @@ public class ClienteNewDTO implements Serializable {
     return nome;
   }
 
-
-
-
   public void setNome(String nome) {
     this.nome = nome;
   }
-
-
-
 
   public String getEmail() {
     return email;
   }
 
-
-
-
   public void setEmail(String email) {
     this.email = email;
   }
-
-
-
 
   public String getCpfOuCnpj() {
     return cpfOuCnpj;
   }
 
-
-
-
   public void setCpfOuCnpj(String cpfOuCnpj) {
     this.cpfOuCnpj = cpfOuCnpj;
   }
-
-
-
 
   public Integer getTipo() {
     return tipo;
   }
 
-
-
-
   public void setTipo(Integer tipo) {
     this.tipo = tipo;
   }
-
-
-
 
   public String getLogradouro() {
     return logradouro;
   }
 
-
-
-
   public void setLogradouro(String logradouro) {
     this.logradouro = logradouro;
   }
-
-
-
 
   public String getNumero() {
     return numero;
   }
 
-
-
-
   public void setNumero(String numero) {
     this.numero = numero;
   }
-
-
-
 
   public String getComplemento() {
     return complemento;
   }
 
-
-
-
   public void setComplemento(String complemento) {
     this.complemento = complemento;
   }
-
-
-
 
   public String getBairro() {
     return bairro;
   }
 
-
-
-
   public void setBairro(String bairro) {
     this.bairro = bairro;
   }
-
-
-
 
   public String getCep() {
     return cep;
   }
 
-
-
-
   public void setCep(String cep) {
     this.cep = cep;
   }
-
-
-
 
   public String getTelefone1() {
     return telefone1;
   }
 
-
-
-
   public void setTelefone1(String telefone1) {
     this.telefone1 = telefone1;
   }
-
-
-
 
   public String getTelefone2() {
     return telefone2;
   }
 
-
-
-
   public void setTelefone2(String telefone2) {
     this.telefone2 = telefone2;
   }
-
-
-
 
   public String getTelefone3() {
     return telefone3;
   }
 
-
-
-
   public void setTelefone3(String telefone3) {
     this.telefone3 = telefone3;
   }
-
-
-
 
   public Integer getCidadeId() {
     return cidadeId;
   }
 
-
-
-
   public void setCidadeId(Integer cidadeId) {
     this.cidadeId = cidadeId;
   }
-  
   
 }
